c6c028ad78f245cbedf05607c6b1e72580e73a0a
Sanchay-Goel <sanchay.17je003408@cse.iitism.ac.in>
2020-09-04 19:49:23 +0530

    Updates

---
+++ precache-manifest.9802ca93f800212ace89e7740967e51e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3227b70331568b60b982b81accf91db7",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "dd7ba4e538cc36ba21aa",
+    "url": "/Weather-app/static/css/main.073a9a4e.chunk.css"
+  },
+  {
+    "revision": "ddd809b82a7d424257e1",
+    "url": "/Weather-app/static/js/2.fd9c0186.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-app/static/js/2.fd9c0186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd7ba4e538cc36ba21aa",
+    "url": "/Weather-app/static/js/main.1b236392.chunk.js"
+  },
+  {
+    "revision": "617b2ce9a85e9885c0e6",
+    "url": "/Weather-app/static/js/runtime-main.33b7e322.js"
+  },
+  {
+    "revision": "d318a635e06f1124dd0b0b5ee5d05832",
+    "url": "/Weather-app/static/media/background.d318a635.jpg"
+  },
+  {
+    "revision": "3023f800098609914a619e292ddd2b99",
+    "url": "/Weather-app/static/media/images.3023f800.png"
+  }
+]);
